8bc6edee9c8c742f6b9d6cf27c85b2f0cd97ebdd
alsongarbuja <garbuja12alson@gmail.com>
2023-03-18 14:33:57 +0545

    WIP: test function catch error but still need work

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
-
-/**
- * 
- * @param name name of the suite
- * @param fn function to be executed
- */
-// export const suite = (name: string, fn: Function) => {
-//     fn();
-// }
+#!/usr/bin/env node
+import { coloredText, formattedText } from "./helpers/cli/chalk-cli.js";
 
 /**
  * 
@@ -14,5 +7,12 @@
  * @param fn function to be executed
  */
 export const test = (name: string, fn: Function) => {
-    fn();
+    // numberOfTests++;
+    try {
+        fn();
+    } catch (error) {
+        console.log(coloredText(`Test ${formattedText(`(${name})`).bold()} Failed`).error());
+        console.log('\n');
+        console.log(error);
+    }
 }
