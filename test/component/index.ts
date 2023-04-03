75d6c56e220a6c69e6c3c5bfeece1cac783d186f
Salipa-Gurung <salipagurung@gmail.com>
2023-04-03 20:05:35 +0545

    feat: component function for v 0.1

---
+++ test/component/index.ts
@@ -0,0 +1,10 @@
+import { component } from "../../src/packages/component/index.js";
+
+const divElement = document.createElement("div");
+divElement.style.color = "blue";
+divElement.style.border = "1px solid black";
+
+component(divElement).haveStyle({
+  color: "blue",
+  border: "1px solid black",
+});
