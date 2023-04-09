8bc27762b568ff641e05438a76c3263899a38016
alsongarbuja <garbuja12alson@gmail.com>
2023-04-09 20:34:31 +0545

    relocate: relocate index.ts back to src folder

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+export { expect } from './packages/expect/index.js';
+export { call } from './packages/call/index.js';
+export { component } from './packages/component/index.js';
+export { test } from './global_functions/index.js';
