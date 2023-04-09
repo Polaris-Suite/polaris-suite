8da221e6fcd1873ef7cd181756c2d0c7b569cdbc
alsongarbuja <garbuja12alson@gmail.com>
2023-04-09 18:31:15 +0545

    config: config for direct package access to functions

---
+++ index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+export { expect } from './src/packages/expect/index.js';
+export { call } from './src/packages/call/index.js';
+export { component } from './src/packages/component/index.js';
+export { test } from './src/global_functions/index.js';
