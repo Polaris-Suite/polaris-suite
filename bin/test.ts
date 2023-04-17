b98fe89d18b7a5043999b4a47486964352626a0a
alsongarbuja <garbuja12alson@gmail.com>
2023-04-17 11:32:52 +0545

    fix: fix the 1st conversation in pr #8

--- bin/test.ts
+++ bin/test.ts
@@ -1,12 +1,35 @@
 #!/usr/bin/env node
-import path from "path";
-import shell from "shelljs";
+import { SpawnOptions } from "child_process";
+import { joinPathSep } from "./helper.js";
+import { spawn } from "child_process";
 
 export const run = () => {
+    // correcting the separator for the path given by the users
     const folders = process.argv[2].split("/");
-    const location = [process.cwd(), ...folders].join(path.sep);
+
+    // joining the current path and the path given by the user
+    const finalPath = joinPathSep(process.cwd(), ...folders);
     
-    shell.exec(`node ${location}`);
+    // executing the node command to the path 
+    const command = "node";
+    const args = [finalPath];
+    const options: SpawnOptions = {
+        stdio: ["inherit", "pipe", "pipe"]
+    }
+
+    const childProcess = spawn(command, args, options);
+
+    childProcess.stdout?.on("data", (data) => {
+        console.log(data.toString());
+    });
+
+    childProcess.on('error', (err) => {
+        console.error(`Error starting child process: ${err}`);
+    });
+
+    childProcess.on("close", (code) => {
+        console.log(`child process exited with code ${code}`);
+    })
 }
 
 run();
