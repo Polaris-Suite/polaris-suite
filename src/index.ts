1f50255b9ce81593f473399d3c9fbb869c244337
alsongarbuja <garbuja12alson@gmail.com>
2023-05-23 22:48:09 +0545

    fix: fix module not found error

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,3 @@ export { call } from './packages/call/index.js';
 export { component } from './packages/component/index.js';
 export { test, suite } from './global_functions/index.js';
 export { launch, goto } from './packages/page/index.js';
-export { After, AfterAll, Before, BeforeAll } from './packages/automation/index.js';
