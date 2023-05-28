e6e774b1406e046fc44ace438a471407d3dae51d
alsongarbuja <garbuja12alson@gmail.com>
2023-05-28 14:28:47 +0545

    feat: complete api testing utility function

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { call } from './packages/call/index.js';
 export { component } from './packages/component/index.js';
 export { test, suite } from './global_functions/index.js';
 export { launch, goto } from './packages/page/index.js';
+export { api } from './packages/api/index.js';
--- src/packages/api/index.ts
+++ src/packages/api/index.ts
@@ -1,6 +1,6 @@
-import { bgColor } from "../../helpers/cli/cli-formatting";
+import { bgColor, coloredText, formattedText } from "../../helpers/cli/cli-formatting";
 import { CustomError, expectMessage } from "../../helpers/error";
-import axios, { Axios, AxiosHeaders, AxiosRequestConfig } from "axios";
+import axios, { AxiosHeaders, AxiosRequestConfig, AxiosResponse } from "axios";
 
 type methodTypes = "POST" | "PUT" | "GET" | "DELETE" | "PATCH";
 
@@ -9,7 +9,7 @@ export const api = async (
   method: methodTypes,
   payload?: null | BodyInit,
   header?: AxiosHeaders
-) => {
+): Promise<APIResult> => {
   let options: AxiosRequestConfig<any> = {
     url,
     method,
@@ -18,10 +18,16 @@ export const api = async (
   if (payload) {
     options.data = payload;
   }
-  const response = await axios(options);
+  let hasError = false;
+
+  const response: dynamicObject = await axios(options).then((res) => res).catch((err) => {
+    hasError = true;
+    return err.response;
+  });
+
   return {
-    statusCode: async (code: number) => {
-      const status = await response.status;
+    statusCode: (code: number) => {
+      const status = response.status;
       if (code !== status) {
         throw new CustomError(
           bgColor("Fail").error(),
@@ -29,6 +35,81 @@ export const api = async (
         );
       }
     },
-    // hasResponse: () => {},
+    hasResponse: (payload: dynamicObject) => {
+      const data = response.data;
+      
+      const keys = Object.keys(payload);
+      keys.forEach((key) => {
+        if (!data.hasOwnProperty(key)) {
+          throw new CustomError(
+            bgColor("Fail").error(),
+            `Expected "${formattedText(key).bold()}" to be in response but it was not found`
+          );
+        } else {
+          if (data[key] !== payload[key]) {
+            throw new CustomError(
+              bgColor("Fail").error(),
+              `Expected "${formattedText(key).bold()}" to be "${formattedText(coloredText(payload[key]).success()).underline()}" but it was "${formattedText(coloredText(data[key]).error()).underline()}"`
+            );
+          }
+        }
+      });
+    },
+    throwsError: (message?: string) => {
+      const errorMessage = response.statusText;
+
+      if(!hasError) {
+        throw new CustomError(
+          bgColor("Fail").error(),
+          `Expected to throw error but it did not`
+        );
+      }
+      if (message && errorMessage !== message) {
+        throw new CustomError(
+          bgColor("Fail").error(),
+          `Expected error to be "${formattedText(coloredText(message).success()).underline()}" but it was "${formattedText(coloredText(errorMessage).error()).underline()}"`
+        );
+      }
+    },
+    not: {
+      statusCode: (code: number) => {
+        const status = response.status;
+        if (code === status) {
+          throw new CustomError(
+            bgColor("Fail").error(),
+            `${expectMessage(`not to be ${code.toString()}`, status.toString())}`
+          );
+        }
+      },
+      hasResponse: (payload: dynamicObject) => {
+        const data = response.data;
+        
+        const keys = Object.keys(payload);
+        keys.forEach((key) => {
+          if (data.hasOwnProperty(key)) {
+            throw new CustomError(
+              bgColor("Fail").error(),
+              `Expected "${formattedText(key).bold()}" not to be in response but it was found`
+            );
+          }
+        });
+      },
+      throwsError: (message?: string) => {
+        const errorMessage = response.statusText;
+  
+        if(hasError) {
+          throw new CustomError(
+            bgColor("Fail").error(),
+            `Expected not to throw error but it did`
+          );
+        }
+        if(message && message === errorMessage) {
+          throw new CustomError(
+            bgColor("Fail").error(),
+            `Expected error not to be "${formattedText(coloredText(message).success()).underline()}" but it was`
+          );
+        }
+      },
+    }
   };
 };
